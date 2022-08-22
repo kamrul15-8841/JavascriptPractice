e3e41abef261890312eb75c408ef9f38ce36b67e
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-08-22 21:56:20 +0600

    1

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const box = document.getElementById("box");
+box.innerHTML = "Md. Kamrul Hasan";
+// box.innerText = "kamrul";
+// var name = "Kamrul Hasan";
+// alert("My Name is : "+name);    
+// console.log("Hello I am Kamrul");
+// console.warn("Hello I am Kamrul");
+// console.error("Hello I am Kamrul");
+// var name = "kamrul";
+// console.log(name);
+//Different Types of Data types 
+// const str = "A String";
+// const num = 0000;
+// const flt = 0.003;
+// const bool = true;
+// const list = ["Item 1",2,"Three", 2.55];
+// const notDefined = undefined;
+// const nothing = null;
+// const obj = {
+//     'name':'Kamrul',
+//     'age':25,
+//     'height': 5,
+// }
